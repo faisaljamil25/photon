32ed4856b01a7efd09b1023f0d16286027d9407e
Faisal Jamil <faisaljamilcs@gmail.com>
2020-10-29 21:42:41 +0530

    chore: refactor code

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const auth = "563492ad6f91700001000001c84723fce83d46c993eddb350985e0bc";
-const url = "https://api.pexels.com/v1/curated?per_page=15&page=1";
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
@@ -16,7 +15,7 @@ function updateInput(event) {
     searchValue = event.target.value;
 }
 
-async function curatedPhotos() {
+async function fetchApi(url) {
     const dataFetch = await fetch(url, {
         method: "GET",
         headers: {
@@ -25,7 +24,10 @@ async function curatedPhotos() {
         }
     });
     const data = await dataFetch.json();
-    console.log(data);
+    return data;
+}
+
+function generatePictures(data) {
     data.photos.forEach(photo => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
@@ -35,23 +37,16 @@ async function curatedPhotos() {
     });
 }
 
-async function searchPhotos(query) {
-    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            Authorization: auth
-        }
-    });
-    const data = await dataFetch.json();
+async function curatedPhotos() {
+    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
     console.log(data);
-    data.photos.forEach(photo => {
-        const galleryImg = document.createElement('div');
-        galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `<img src="${photo.src.large}"/>
-        <p>${photo.photographer}</p>`;
-        gallery.appendChild(galleryImg);
-    });
+    generatePictures(data);
+}
+
+async function searchPhotos(query) {
+    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`);
+    console.log(data);
+    generatePictures(data);
 }
 
 curatedPhotos();

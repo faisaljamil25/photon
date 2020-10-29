33b9ccaad018edaf354324e14d7a2e3d6f02d77b
Faisal Jamil <faisaljamilcs@gmail.com>
2020-10-29 21:25:07 +0530

    feat: search photos

--- app.js
+++ app.js
@@ -2,25 +2,21 @@ const auth = "563492ad6f91700001000001c84723fce83d46c993eddb350985e0bc";
 const url = "https://api.pexels.com/v1/curated?per_page=15&page=1";
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
-const submitBtn = document.querySelector('.submit-btn');
-let searchValue = searchInput.value;
+const form = document.querySelector('.search-form');
+let searchValue;
 
-submitBtn.addEventListener('click', searchPhotos(searchValue))
+// Event Listners
+searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    searchPhotos(searchValue);
+})
+
+function updateInput(event) {
+    searchValue = event.target.value;
+}
 
 async function curatedPhotos() {
-    const data = await getPhotos(url);
-    console.log(data);
-    imgGallery(data);
-}
-
-async function searchPhotos(query) {
-    const searchUrl = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
-    const data = await getPhotos(searchUrl);
-    imgGallery(data);
-}
-
-
-async function getPhotos(url) {
     const dataFetch = await fetch(url, {
         method: "GET",
         headers: {
@@ -29,10 +25,7 @@ async function getPhotos(url) {
         }
     });
     const data = await dataFetch.json();
-    return data;
-}
-
-function imgGallery(data) {
+    console.log(data);
     data.photos.forEach(photo => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
@@ -42,5 +35,23 @@ function imgGallery(data) {
     });
 }
 
+async function searchPhotos(query) {
+    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: auth
+        }
+    });
+    const data = await dataFetch.json();
+    console.log(data);
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement('div');
+        galleryImg.classList.add('gallery-img');
+        galleryImg.innerHTML = `<img src="${photo.src.large}"/>
+        <p>${photo.photographer}</p>`;
+        gallery.appendChild(galleryImg);
+    });
+}
 
-// curatedPhotos();
+curatedPhotos();
